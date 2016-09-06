e3fc2d02d3daef6befd53b61b598ac9d2b29194f
Jurriaan Den Toonder <speedy4104@hotmail.com>
2016-09-06 10:37:59 +0200

    Add simple test

---
+++ src/test/java/net/liquidpineapple/pang/ApplicationTest.java
@@ -0,0 +1,18 @@
+package net.liquidpineapple.pang;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Jurriaan Den Toonder<jurriaan.toonder@liquidpineapple.net>
+ * @date 06 09 2016.
+ */
+public class ApplicationTest {
+
+    @Test
+    public void testStartup() throws Throwable {
+        Application app = new Application();
+    }
+
+}
