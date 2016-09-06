926cf1d816decadbf369db1b45200c0ef1f37efe
Jurriaan Den Toonder <speedy4104@hotmail.com>
2016-09-06 10:23:55 +0200

    Add the main frame

---
+++ src/main/java/Application.java
@@ -0,0 +1,63 @@
+import gui.Board;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * The main application, extends {@link JFrame}
+ * @author Jurriaan Den Toonder<jurriaan.toonder@liquidpineapple.net>
+ * @date 06 09 2016.
+ */
+@Slf4j
+public class Application extends JFrame {
+
+    private static String PROPERTIES_LOCATION = "/config.properties";
+
+    private Properties properties;
+
+    public Application() throws IOException {
+        log.info("Starting application...");
+        InputStream stream = this.getClass().getResourceAsStream(PROPERTIES_LOCATION);
+        if (stream != null) {
+            properties = new Properties();
+            properties.load(stream);
+        } else {
+            String err = "Could not find resource file: " + PROPERTIES_LOCATION;
+            throw new FileNotFoundException(err);
+        }
+        initGUI();
+    }
+
+    private void initGUI() {
+        add(new Board());
+
+        Integer width = Integer.valueOf(properties.getProperty("application-width"));
+        Integer height = Integer.valueOf(properties.getProperty("application-height"));
+        String name = properties.getProperty("application-name");
+
+        setSize(width, height);
+
+        setTitle(name);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+            Application ex;
+            try {
+                ex = new Application();
+                ex.setVisible(true);
+            } catch (IOException e) {
+                log.error(e.getMessage(), e);
+            }
+        });
+
+    }
+}
---
+++ src/main/java/gui/Board.java
@@ -0,0 +1,12 @@
+package gui;
+
+import javax.swing.*;
+
+/**
+ * @author: Jurriaan Den Toonder<jurriaan.toonder@liquidpineapple.net>
+ * @date: 06 09 2016.
+ */
+public class Board extends JPanel {
+
+    public Board() {}
+}
