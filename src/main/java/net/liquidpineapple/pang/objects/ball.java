46f5631ee4c0ee4f496c3f40c8fcd02a5efbf098
Erik Wiegel <P.D.Wiegel@student.tudelft.nl>
2016-09-07 09:08:27 +0200

    Added proper javadoc and reshuffled tests to be more clear

--- src/main/java/net/liquidpineapple/pang/objects/ball.java
+++ src/main/java/net/liquidpineapple/pang/objects/ball.java
@@ -1,7 +1,93 @@
 package net.liquidpineapple.pang.objects;
 
+import java.lang.reflect.Array;
+import java.util.Random;
+
+import static java.lang.Math.random;
+import static javafx.scene.input.KeyCode.X;
+import static javafx.scene.input.KeyCode.Y;
+
 /**
  * Created by Erik on 7-9-2016.
  */
-public class ball {
+public class Ball {
+
+    //Size 1 is the smallest ball.
+    int size;
+    int movX;
+    int movY;
+
+    /**
+     * Creates a ball with a set horizontal speed and a variable vertical speed depending on size.
+     *
+     * @param spawnX    X-coordinate to make the ball
+     * @param spawnY    Y-coordinate to make the ball
+     * @param direction Direction the ball should move, valid inputs are: "left" and "right", anything else randomizes the direction.
+     * @param sizeIn    The desired size of the ball, Greater then 4 or smaller then 0 results in a size 4 ball.
+     */
+    public Ball(int spawnX, int spawnY, String direction, int sizeIn){
+        if(sizeIn>0 && sizeIn<5){
+        size = sizeIn;}
+        else{size = 4;}
+        //Makes the ball go either left or right depending on input, if the input is incorrect it randomly chooses a direction.
+        if(direction=="left"){movX=-15;}
+        else if(direction=="right"){movX=15;}
+        else {Random coinflip = new Random();
+            if(coinflip.nextInt(1)==1){movX=-15;}
+            else {movX=15;}}
+        movY = 3*size+5;
+        //Create actual ball here.
+    }
+
+    /**
+     * Calculates and returns the next position the ball should be drawn in.
+     * !!currently not returing
+     */
+    public void nextMovement(){
+        //int new X = selfX + movX;
+        movY += 2;
+        //int new Y = selfY + movY;
+        //return (0,0);
+    }
+
+    /**
+     * Handles floor colission, speed is set such that it always goes to the same height from the floor bounced on. hitting a higher platform means ending up higher too.
+     */
+    void collidefloor(){
+        //This way it always goes to the same height after hitting a surface.
+        movY = -(3*size+5);
+    }
+
+    /**
+     * Handles wall colission by negating the horizontal direction.
+     */
+    void collidewall(){
+        //We always move the same speed so we can just change direction.
+        movX = -movX;
+    }
+
+    /**
+     * Adds score (WIP), spawns two balls going left and right respectively on itself, and removes this ball.
+     */
+    void destroyball(){
+        //setscore+100
+        if(size!=1){
+            //ball(selfX, selfY, "left", size-1);
+            //ball(selfX, selfY, "right", size-1);
+        }
+        //remove ball
+    }
+
+    public int getSize(){
+        return size;
+    }
+
+    public int getMovX(){
+        return movX;
+    }
+
+    public int getMovY(){
+        return movY;
+    }
+
 }
--- src/test/java/net/liquidpineapple/pang/BallTest.java
+++ src/test/java/net/liquidpineapple/pang/BallTest.java
@@ -1,27 +1,44 @@
 package net.liquidpineapple.pang;
 
+import net.liquidpineapple.pang.objects.Ball;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
-import java.io.FileNotFoundException;
-
-/**
- * @author Jurriaan Den Toonder<jurriaan.toonder@liquidpineapple.net>
- * @date 2016/09/06.
- */
 public class BallTest {
 
-    private static String PROPERTIES_LOCATION = "/config.properties";
+    Ball TestBallLeft = new Ball(0, 0, "left", 4);
+    Ball TestBallRight = new Ball(0, 0, "right", 3);
+    Ball TestBallIncorrectSizeGreater = new Ball(0, 0, "left", 6);
+    Ball TestBallIncorrectSizeSmaller = new Ball(0, 0, "left", 0);
+    Ball TestBallIncorrectDirection = new Ball(0, 0, "Cheese", 3);
 
+    //Tests if the constructor handles left and right correctly.
     @Test
-    public void testStartup() throws Throwable {
-        Application app = new Application(PROPERTIES_LOCATION);
-        app.start();
+    public void testConstructorDirection() throws Throwable {
+        assertEquals(-15, TestBallLeft.getMovX());
+        assertEquals(15, TestBallRight.getMovX());
     }
 
-    @Test(expected = FileNotFoundException.class)
-    public void testWrongProperties() throws Throwable {
-        Application app = new Application("test");
-        app.start();
+    //Tests if the constructor handles size correctly
+    @Test
+    public void testConstructorSize() throws Throwable {
+        assertEquals(4, TestBallLeft.getSize());
+        assertEquals(3, TestBallRight.getSize());
+    }
+
+    //Tests if the constructor handles incorrect sizes correctly
+    @Test
+    public void testIncorrectSize() throws Throwable {
+        assertEquals(4, TestBallIncorrectSizeGreater.getSize());
+        assertEquals(4, TestBallIncorrectSizeSmaller.getSize());
+    }
+
+    //Tests if the constructor handles incorrect directions properly.
+    @Test
+    public void testIncorrectDirection() throws Throwable {
+        assertTrue(TestBallIncorrectDirection.getMovX() == 15 || TestBallIncorrectDirection.getMovX() == -15);
     }
 
 }
