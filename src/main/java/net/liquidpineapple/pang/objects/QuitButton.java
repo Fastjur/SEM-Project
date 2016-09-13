16a18a412dd52ae0e4f92d6958e6c0eb22d7940d
Jaap-Jan <J.J.vanderSteeg@student.tudelft.nl>
2016-09-13 09:13:10 +0200

    Make the buttons only fire once, fixes #30

--- src/main/java/net/liquidpineapple/pang/objects/QuitButton.java
+++ src/main/java/net/liquidpineapple/pang/objects/QuitButton.java
@@ -1,10 +1,18 @@
 package net.liquidpineapple.pang.objects;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * Created by Jaap-Jan on 9-9-2016.
  */
+@Slf4j
 public class QuitButton extends Button{
     public QuitButton(int startX, int startY) {
         super("/images/quit.png", startX, startY);
+    }
+
+    @Override
+    public void onClick() {
+        log.info("Close");
     }
 }
