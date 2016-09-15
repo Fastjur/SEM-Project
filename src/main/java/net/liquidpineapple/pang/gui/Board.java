ddf90b4f99c5491d165824f0affc5e287d34f7d1
Jaap-Jan <J.J.vanderSteeg@student.tudelft.nl>
2016-09-15 10:07:33 +0200

    Fixed mouselistener, fixes #51

--- src/main/java/net/liquidpineapple/pang/gui/Board.java
+++ src/main/java/net/liquidpineapple/pang/gui/Board.java
@@ -3,6 +3,7 @@ package net.liquidpineapple.pang.gui;
 import lombok.extern.slf4j.Slf4j;
 import net.liquidpineapple.pang.InputHandler;
 import net.liquidpineapple.pang.screens.Level;
+import net.liquidpineapple.pang.screens.MainMenu;
 import net.liquidpineapple.pang.screens.Screen;
 
 import javax.swing.*;
@@ -29,6 +30,7 @@ public class Board extends JPanel {
         setFocusable(true);
         requestFocus();
         addKeyListener(new InputHandler());
+        addMouseListener(new InputHandler());
         init();
     }
 
@@ -36,8 +38,8 @@ public class Board extends JPanel {
         setFocusable(true);
         setBackground(Color.BLACK);
 
-        currentScreen = Level.createFromXML("src/main/resources/levels/level1.xml");
-//        currentScreen = new MainMenu();
+       // currentScreen = Level.createFromXML("src/main/resources/levels/level1.xml");
+        currentScreen = new MainMenu();
     }
 
     public void changeScreen(Screen screen){
