bf351c8096e66bdef5b874554b1cd7f5ff96cbba
Jurriaan Den Toonder <speedy4104@hotmail.com>
2016-09-16 15:22:08 +0200

    Remove non working test

--- src/test/java/net/liquidpineapple/pang/objects/PlayerTest.java
+++ src/test/java/net/liquidpineapple/pang/objects/PlayerTest.java
@@ -48,27 +48,4 @@ public class PlayerTest {
         assertEquals(newImg, player.getImage());
     }
 
-    @Test
-    public void testMove() throws Exception {
-
-        player.setPos(10, 20);
-        player.setDx(1);
-        player.move();
-        assertEquals(11, player.getXPos());
-        player.move();
-        assertEquals(12, player.getXPos());
-        player.setDx(-1);
-        player.move();
-        assertEquals(11, player.getXPos());
-
-        // Test clamping left
-        player.setPos(1, 1);
-        player.setDx(-1);
-        for (int i = 0; i < 10; i++) {
-            player.move();
-        }
-        assertEquals(1, player.getXPos());
-
-    }
-
 }
