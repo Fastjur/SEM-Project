99bf0368fe7cc8914da0f1322cd9a079035c4990
Govert de Gans <grappigegovert@hotmail.com>
2016-09-22 23:22:56 +0200

    Adds looping background music to the game

---
+++ src/main/java/net/liquidpineapple/pang/AudioSystem.java
@@ -0,0 +1,52 @@
+package net.liquidpineapple.pang;
+
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.Player;
+import net.liquidpineapple.pang.logger.Logger;
+
+import java.io.*;
+
+/**
+ * @author Govert de Gans
+ * @date 2016-09-22
+ */
+public class AudioSystem {
+
+    private static Player player;
+    private static String loopingSound;
+
+    public static void start() {
+        new Thread(() -> {
+            playLoop();
+        }).start();
+    }
+
+    private static void playLoop() {
+        Logger.info("Music loop is running");
+        byte[] buffer = null;
+        try {
+            String currentLoop = "";
+            while (true) {
+                if (!currentLoop.equals(loopingSound)) {
+                    BufferedInputStream resourceStream = new BufferedInputStream(
+                        Application.class.getResourceAsStream(loopingSound));
+                    buffer = new byte[resourceStream.available()];
+                    resourceStream.read(buffer);
+                    currentLoop = loopingSound;
+                }
+                player = new Player(new ByteArrayInputStream(buffer));
+                player.play();
+            }
+        } catch(JavaLayerException e) {
+            Logger.error("The AudioSystem has crashed.", e);
+        } catch(IOException e) {
+            Logger.error("The AudioSystem has crashed.", e);
+        }
+    }
+
+    public static void changeLoopingSound(String newLoopingSound) {
+        if (loopingSound != null)
+            player.close();
+        loopingSound = newLoopingSound;
+    }
+}
