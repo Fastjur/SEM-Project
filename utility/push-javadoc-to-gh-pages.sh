36ada6c4767815ede4fbd47eec7a2da63bc5fb20
Jurriaan Den Toonder <speedy4104@hotmail.com>
2016-09-27 13:17:30 +0200

    Hotfix page generation
    Had to be on master, sorry

--- utility/push-javadoc-to-gh-pages.sh
+++ utility/push-javadoc-to-gh-pages.sh
@@ -12,9 +12,8 @@ if [ "$TRAVIS_REPO_SLUG" == "Fastjur/SEM-Project" ] && [ "$TRAVIS_JDK_VERSION" =
   git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/Fastjur/SEM-Project gh-pages > /dev/null
 
   cd gh-pages
-  git rebase master -f
-  git rm -rf ./docs/site
-  cp -Rf $HOME/site-latest ./docs/site
+  git rm -rf ./site
+  cp -Rf $HOME/site-latest ./site
   git add -f .
   git commit -m "Latest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
   git push -fq origin gh-pages > /dev/null
