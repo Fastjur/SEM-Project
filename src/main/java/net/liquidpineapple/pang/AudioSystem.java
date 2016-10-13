75d138148a8302f37adb8c0602c1e7fb111db13f
Govert de Gans <grappigegovert@hotmail.com>
2016-10-14 00:36:39 +0200

    Add function for playing sound effects

--- src/main/java/net/liquidpineapple/pang/AudioSystem.java
+++ src/main/java/net/liquidpineapple/pang/AudioSystem.java
@@ -2,11 +2,17 @@ package net.liquidpineapple.pang;
 
 import javazoom.jl.decoder.JavaLayerException;
 import javazoom.jl.player.Player;
+
 import net.liquidpineapple.pang.logger.Logger;
 
 import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineEvent;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 /**
  * Audiosystem for the application. Plays the background music.
@@ -77,6 +83,23 @@ public class AudioSystem {
       player.close();
     } catch (NullPointerException ex) {
       Logger.warning("Attempted to close player but was null");
+    }
+  }
+
+  public static void playEffect(String path) {
+    try {
+      Logger.info("Playing sound effect " + path);
+      Clip clip = javax.sound.sampled.AudioSystem.getClip();
+      clip.open(javax.sound.sampled.AudioSystem.getAudioInputStream(Application.class.getResourceAsStream(path)));
+      clip.addLineListener((lineEvent) -> {
+        if (lineEvent.getType() == LineEvent.Type.STOP)
+          clip.close(); // close resources when clip is finished.
+      });
+      clip.start();
+    } catch (LineUnavailableException | IOException e) {
+      Logger.error("Could not play sound effect", e);
+    } catch (UnsupportedAudioFileException e) {
+      Logger.error("The AudioSystem tried to play an unsupported audio file", e);
     }
   }
 }
