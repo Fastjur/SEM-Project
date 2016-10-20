e304e47c9b7c35c9ed452a1641702bcf8aee44b0
Tim Rietveld <T.M.rietveld@student.tudelft.nl>
2016-10-20 12:45:12 +0200

    added properties to transformer to create better readable xml-file

--- src/main/java/net/liquidpineapple/pang/XmlHandler.java
+++ src/main/java/net/liquidpineapple/pang/XmlHandler.java
@@ -27,6 +27,7 @@ import javax.imageio.ImageIO;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
@@ -83,6 +84,8 @@ public class XmlHandler {
       // write the content into xml file
       TransformerFactory transformerFactory = TransformerFactory.newInstance();
       Transformer transformer = transformerFactory.newTransformer();
+      transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+      transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
       DOMSource source = new DOMSource(doc);
       StreamResult result = new StreamResult(new File(
           LevelScreen.class.getResource("/levels/customlevel/level1.xml").getFile()));
