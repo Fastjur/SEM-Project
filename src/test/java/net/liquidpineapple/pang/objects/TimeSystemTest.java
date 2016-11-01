d84ecc7e0d90a4a8a68785f43dbdb24c18d2459d
Erik Wiegel <P.D.Wiegel@student.tudelft.nl>
2016-11-01 13:22:25 +0100

    added tests for Timesystem

--- src/test/java/net/liquidpineapple/pang/objects/TimeSystemTest.java
+++ src/test/java/net/liquidpineapple/pang/objects/TimeSystemTest.java
@@ -1,11 +1,15 @@
 package net.liquidpineapple.pang.objects;
 
-import static org.junit.Assert.assertEquals;
-
-import net.liquidpineapple.pang.gui.LifeSystem;
-
+import net.liquidpineapple.pang.gui.NumberToken;
+import net.liquidpineapple.pang.gui.TimeSystem;
 import org.junit.Before;
 import org.junit.Test;
+import org.powermock.reflect.Whitebox;
+
+
+import java.sql.Time;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Erik on 13-9-2016.
@@ -14,39 +18,55 @@ public class TimeSystemTest {
 
   @Before
   public void setUp() {
-    LifeSystem.getInstance();
+    TimeSystem.getInstance();
+    TimeSystem.start();
+
   }
 
   @Test
-  public void gainliveat3() {
-    while (LifeSystem.getLives() < 3) {
-      LifeSystem.gainLife();
-    }
-    LifeSystem.gainLife();
-    assertEquals(LifeSystem.getLives(), 3);
+  public void TestsetFrozenTime() {
+    assertEquals(0, TimeSystem.getFrozen());
+    TimeSystem.setFrozen(10);
+    assertEquals(10, TimeSystem.getFrozen());
   }
 
   @Test
-  public void loseliveat3Andthengain() {
-    while (LifeSystem.getLives() < 3) {
-      LifeSystem.gainLife();
-    }
-    LifeSystem.loseLife();
-    assertEquals(LifeSystem.getLives(), 2);
-    LifeSystem.gainLife();
-    assertEquals(LifeSystem.getLives(), 3);
+  public void TeststopTime() {
+    TimeSystem.stop();
+    int oldTime = TimeSystem.getTime();
+    assertEquals(oldTime, TimeSystem.getTime());
+    TimeSystem.start();
   }
 
   @Test
-  public void loseliveAndReset() {
-    while (LifeSystem.getLives() < 3) {
-      LifeSystem.gainLife();
+  public void TestResetTime() {
+    TimeSystem.stop();
+    TimeSystem.setTime(25);
+    TimeSystem.resetTime(111);
+    for (NumberToken token : TimeSystem.getTimePlaces()) {
+      assertEquals(1, token.getCurrentnumber());
     }
-    LifeSystem.loseLife();
-    assertEquals(LifeSystem.getLives(), 2);
-    LifeSystem.getInstance().resetLives();
-    assertEquals(LifeSystem.getLives(), 3);
+    TimeSystem.start();
   }
 
+  @Test
+  //Tests if it properly handles time > 999
+  public void TestResetTimeOver999() {
+    TimeSystem.stop();
+    TimeSystem.setTime(25);
+    TimeSystem.resetTime(1110);
+    for (NumberToken token : TimeSystem.getTimePlaces()) {
+      assertEquals(9, token.getCurrentnumber());
+    }
+    TimeSystem.start();
+  }
+
+  @Test
+  public synchronized void TestTimeUp() throws Exception {
+    TimeSystem.stop();
+    TimeSystem.setTime(0);
+    Whitebox.invokeMethod(TimeSystem.getInstance(), "timeTick");
+    assertEquals(60, TimeSystem.getTime());
+  }
 
 }
