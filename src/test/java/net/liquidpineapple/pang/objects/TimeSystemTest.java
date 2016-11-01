5f00391fd56f282405de0c3dd5418582c61e8aa8
Erik Wiegel <P.D.Wiegel@student.tudelft.nl>
2016-11-01 13:22:20 +0100

    added tests for Timesystem

---
+++ src/test/java/net/liquidpineapple/pang/objects/TimeSystemTest.java
@@ -0,0 +1,52 @@
+package net.liquidpineapple.pang.objects;
+
+import static org.junit.Assert.assertEquals;
+
+import net.liquidpineapple.pang.gui.LifeSystem;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by Erik on 13-9-2016.
+ */
+public class TimeSystemTest {
+
+  @Before
+  public void setUp() {
+    LifeSystem.getInstance();
+  }
+
+  @Test
+  public void gainliveat3() {
+    while (LifeSystem.getLives() < 3) {
+      LifeSystem.gainLife();
+    }
+    LifeSystem.gainLife();
+    assertEquals(LifeSystem.getLives(), 3);
+  }
+
+  @Test
+  public void loseliveat3Andthengain() {
+    while (LifeSystem.getLives() < 3) {
+      LifeSystem.gainLife();
+    }
+    LifeSystem.loseLife();
+    assertEquals(LifeSystem.getLives(), 2);
+    LifeSystem.gainLife();
+    assertEquals(LifeSystem.getLives(), 3);
+  }
+
+  @Test
+  public void loseliveAndReset() {
+    while (LifeSystem.getLives() < 3) {
+      LifeSystem.gainLife();
+    }
+    LifeSystem.loseLife();
+    assertEquals(LifeSystem.getLives(), 2);
+    LifeSystem.getInstance().resetLives();
+    assertEquals(LifeSystem.getLives(), 3);
+  }
+
+
+}
